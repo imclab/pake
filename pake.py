6f0e85fe22e73375abd9d7025b2e0cb75e73b83d
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2013-04-02 19:09:15 +0200

    Add which() function to pake.py

--- pake.py
+++ pake.py
@@ -366,3 +366,21 @@ def virtual(name, *dependencies, **kwargs):
     target = Target(name, dependencies=dependencies, clean=False, phony=True,
                     **kwargs)
     targets.add(target)
+
+
+def which(program):
+    """Returns the full path of a given argument or `None`.
+    See: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python"""
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
