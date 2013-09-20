7c4c6c3fcbc949cdddb451ef5db6efb2f1f7f78b
Tom Payne <twpayne@gmail.com>
2013-09-20 10:02:10 +0100

    Add help text to build.py

--- build.py
+++ build.py
@@ -9,12 +9,12 @@ SRC = [path for path in ifind('.') if path.endswith('.py')]
 virtual('all', 'pep8', 'pyflakes')
 
 
-@target('pep8', SRC, phony=True)
+@target('pep8', SRC, help='runs pep8 on all source files', phony=True)
 def pep8(t):
     t.run('pep8', '--ignore=E501', SRC)
 
 
-@target('pyflakes', SRC, phony=True)
+@target('pyflakes', SRC, help='runs pyflakes on all source files', phony=True)
 def pyflakes(t):
     t.run('pyflakes', SRC)
 
